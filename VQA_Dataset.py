cbafa771103130cca2997ae2a5a45edbe09f760b
JansSolanoVega <janssv.200604@gmail.com>
2023-11-15 14:29:43 +0100

    return answers and questions text

--- VQA_Dataset.py
+++ VQA_Dataset.py
@@ -7,7 +7,7 @@ import json
 from PIL import Image
 
 
-class VQA_Dataset(datasets.Dataset):
+class VQA_Dataset(torch.utils.data.Dataset):
     
     def __init__(self):
         
@@ -17,8 +17,7 @@ class VQA_Dataset(datasets.Dataset):
         self.image_ids = []
         self.question_tokens = []
         self.answer_tokens = []  
-        self.correct_answers = []  
-        
+        self.correct_answers = [] 
             # image = preprocess(Image.open("CLIP.png")).unsqueeze(0).to(device)
         
     def load_all(self, preprocess, device, length=100):
@@ -39,24 +38,18 @@ class VQA_Dataset(datasets.Dataset):
                     answers_text = question['multiple_choices']
 
                     # find quesion id and image id in annotations
-                    found = False
                     for annotation in annntoations:
                         if annotation['question_id'] == question_id and annotation['image_id'] == image_id:
                             correct_answer = annotation['multiple_choice_answer']
                             # get the index of the answer in the list of answers
-                            annotation_answers = annotation['answers']
-                            for answer_info in annotation_answers:
-                                answer = answer_info['answer']
-                                if answer == correct_answer:
-                                    index = answer_info['answer_id'] - 1
-                            found = True
-                            break
+                            for i, possible_answer in enumerate(answers_text):
+                                if possible_answer == correct_answer:
+                                    index = i
+                                    break
                         
-                    if not found:
-                        print("ERROR: Could not find answer for question id: ", question_id, " and image id: ", image_id)
-                        continue
+                
 
-                    self.correct_answers.append(torch.tensor(index).unsqueeze(0))
+                    self.correct_answers.append(index)
                     self.image_ids.append(torch.tensor(image_id).unsqueeze(0))
                     self.questions.append(question_text)
                     self.answers.append(answers_text)
@@ -74,20 +67,19 @@ class VQA_Dataset(datasets.Dataset):
     def __len__(self):
         return len(self.questions)
 
-    def __getitem__(self, indices):
-            index = indices[0]
+    def __getitem__(self, index):
             result = {
                 'image': self.images[index],
                 'answer_tokens': self.answer_tokens[index],
                 'question_tokens': self.question_tokens[index],
                 'image_id': self.image_ids[index],
-                'correct_answer': self.correct_answers[index]
+                'correct_answer_idx': self.correct_answers[index],
+                'question': self.questions[index],
+                'possible_answers': self.answers[index],
+                'correct_answer_text': self.answers[index][self.correct_answers[index]],
             }
-            return result
-                #'question': self.questions[index],
-                #'answer': self.answers[index]
-                # cant return question, answer directly because they are strings and not tensors
            
+            return result
     
 
 if __name__ == "__main__":
@@ -98,10 +90,10 @@ if __name__ == "__main__":
     dataset.load_all(preprocess, length=200, device=device)
     dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
     
-    found = False
     for i in tqdm(dataloader, desc="Testing"):
         id = i['image_id']
-        if id == torch.tensor(26216) and not found:
-            found = True
-            print("id: ", id, "question: ", i['question_tokens'], "answer: ", i['answer_tokens'])
+        if id == torch.tensor(26216):
+            print("id: ", id, "question: ", i['question'], "Answer: ", i['correct_answer_text'])
+            print("Possible answers: ", i['possible_answers'])
+            break
         
