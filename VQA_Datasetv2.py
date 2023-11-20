948360b01bc7312545e6645f3d9d97e22ce0d687
Paul <mr_paul_mayer@gmx.de>
2023-11-20 13:17:43 +0100

    added different loading and models

--- VQA_Datasetv2.py
+++ VQA_Datasetv2.py
@@ -1,3 +1,4 @@
+import pickle
 import datasets
 import torch
 from tqdm import tqdm
@@ -17,10 +18,15 @@ class VQA_Dataset(torch.utils.data.Dataset):
         self.image_ids = []
         self.question_tokens = []
         self.answer_tokens = []  
-        self.correct_answers = [] 
+        self.correct_answers = []
+        self.image_features = []
+        self.question_features = []
+        self.answer_features = []
+        self.device = None 
+        self.preprocess = None
             # image = preprocess(Image.open("CLIP.png")).unsqueeze(0).to(device)
         
-    def load_all(self, preprocess, device, length=100):
+    def load_preprocess(self, preprocess, device, length=100):
         
         with open('Annotations/MultipleChoice_abstract_v002_val2015_questions.json', 'r') as question_file:
             with open('Annotations/abstract_v002_val2015_annotations.json', 'r') as answer_file:
@@ -47,8 +53,6 @@ class VQA_Dataset(torch.utils.data.Dataset):
                                     index = i
                                     break
                         
-                
-
                     self.correct_answers.append(index)
                     self.image_ids.append(torch.tensor(image_id).unsqueeze(0))
                     self.questions.append(question_text)
@@ -60,13 +64,111 @@ class VQA_Dataset(torch.utils.data.Dataset):
                     self.images.append(preprocess(Image.open("Images/abstract_v002_val2015_0000000{}.png".format(image_id))).to(device))
                     # can we process all images at once?
                     
+    def load(self, preprocess, device , length=100):
+        self.device = device
+        self.preprocess = preprocess
+        "load without preprocessing or tokenizing"
+        with open('Annotations/MultipleChoice_abstract_v002_val2015_questions.json', 'r') as question_file:
+            with open('Annotations/abstract_v002_val2015_annotations.json', 'r') as answer_file:
+                question_data = json.load(question_file)
+                answer_data = json.load(answer_file)
 
+                annntoations = (answer_data['annotations'])
+                questions = (question_data['questions'])
+
+                for question in tqdm(questions[:length], desc="Loading Images"):
+                    
+                    image_id = question['image_id']
+                    question_text = question['question']
+                    question_id = question['question_id']
+                    answers_text = question['multiple_choices']
+
+                    # find quesion id and image id in annotations
+                    for annotation in annntoations:
+                        if annotation['question_id'] == question_id and annotation['image_id'] == image_id:
+                            correct_answer = annotation['multiple_choice_answer']
+                            # get the index of the answer in the list of answers
+                            for i, possible_answer in enumerate(answers_text):
+                                if possible_answer == correct_answer:
+                                    index = i
+                                    break
+                        
+                    self.correct_answers.append(index)
+                    self.image_ids.append(torch.tensor(image_id).unsqueeze(0))
+                    self.questions.append(question_text)
+                    self.answers.append(answers_text)
+                    
+                    # can we process all images at once?
+
+    def load_encode(self, preprocess, device , model, length=100):
+        self.device = device
+        # preprocess the image and tokenize the question and answers and embbed them
+        with open('Annotations/MultipleChoice_abstract_v002_val2015_questions.json', 'r') as question_file:
+            with open('Annotations/abstract_v002_val2015_annotations.json', 'r') as answer_file:
+                question_data = json.load(question_file)
+                answer_data = json.load(answer_file)
+
+                annntoations = (answer_data['annotations'])
+                questions = (question_data['questions'])
+
+                for question in tqdm(questions[:length], desc="Preprocessing Images"):
+                    
+                    image_id = question['image_id']
+                    question_text = question['question']
+                    question_id = question['question_id']
+                    answers_text = question['multiple_choices']
+
+                    # find quesion id and image id in annotations
+                    for annotation in annntoations:
+                        if annotation['question_id'] == question_id and annotation['image_id'] == image_id:
+                            correct_answer = annotation['multiple_choice_answer']
+                            # get the index of the answer in the list of answers
+                            for i, possible_answer in enumerate(answers_text):
+                                if possible_answer == correct_answer:
+                                    index = i
+                                    break
+                        
+                    self.correct_answers.append(index)
+                    
+                    question_tokens = clip.tokenize([question_text]).to(device)
+                    answer_tokens = clip.tokenize(answers_text).to(device)
+                    image = preprocess(Image.open("Images/abstract_v002_val2015_0000000{}.png".format(image_id))).unsqueeze(0).to(device)
+
+                    
+                    self.image_features.append(model.encode_image(image).to("cpu"))
+                    self.question_features.append(model.encode_text(question_tokens).to("cpu"))
+                    self.answer_features.append(model.encode_text(answer_tokens).to("cpu"))
+                    
+    def save(self, path):
+        # save precalculated features and correct answers in a file
+        if len(self.image_features) != 0:
+            with open(path + 'embeddings.pkl', "wb") as fOut:
+                pickle.dump({'image_features': self.image_features,'answer_features': self.answer_features, 'question_features': self.question_features, 'correct_anwsers': self.correct_answers }, fOut, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def load_saved(self, path):
+        # load precalculated features and correct answers from a file
+        with open(path + 'embeddings.pkl', "rb") as fIn:
+            data = pickle.load(fIn)
+            self.image_features = data['image_features']
+            self.answer_features = data['answer_features']
+            self.question_features = data['question_features']
+            self.correct_answers = data['correct_anwsers']
 
     def __len__(self):
-        return len(self.questions)
+        return len(self.correct_answers)
 
     def __getitem__(self, index):
-        return  self.images[index], self.answer_tokens[index], self.question_tokens[index], self.correct_answers[index]
+        if len(self.images) != 0: 
+            return  self.images[index], self.answer_tokens[index], self.question_tokens[index], self.correct_answers[index]      
+        elif len(self.image_features) != 0:
+            return  self.image_features[index], self.answer_features[index], self.question_features[index], self.correct_answers[index]
+        else:
+            # preprocess the image and tokenize the question and answers
+            image_id = int(self.image_ids[index])
+            image = self.preprocess(Image.open("Images/abstract_v002_val2015_0000000{}.png".format(image_id))).to(self.device)
+            answer_tokens = clip.tokenize(self.answers[index]).to(self.device)
+            question_tokens = clip.tokenize([self.questions[index]]).to(self.device)
+            return  image, answer_tokens, question_tokens, self.correct_answers[index]
     
 
 if __name__ == "__main__":
@@ -74,7 +176,7 @@ if __name__ == "__main__":
     model, preprocess = clip.load("ViT-B/32", device=device)
 
     dataset = VQA_Dataset()
-    dataset.load_all(preprocess, length=200, device=device)
+    dataset.load_preprocess(preprocess, length=200, device=device)
     dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
     
     for i in tqdm(dataloader, desc="Testing"):
--- models.py
+++ models.py
@@ -62,3 +62,124 @@ class VQA_Model2(torch.nn.Module):
             similarity = (100*torch.einsum("bn,bqn->bq", [combined_features, answer_features])) #scaling before softmax
         
         return similarity
+
+class VQA_Model3(torch.nn.Module):
+    """architecture that uses clip and a small NN to combine question and image features to get a embedding of the same size as the answer embedding
+        Only train the NN, not the clip model
+        with relu activation
+    """
+    def __init__(self, model, device):
+        super().__init__()
+        self.model = model
+        self.device = device
+        self.fc1 = torch.nn.Linear(1024, 512).to(self.device)
+        self.initialize_parameters()
+
+    def initialize_parameters(self):
+        # Apply Xavier/Glorot initialization to the linear layer
+        torch.nn.init.xavier_uniform_(self.fc1.weight)
+        torch.nn.init.zeros_(self.fc1.bias.data)
+
+    def forward(self, image, question_tokens, answer_tokens):
+        """returns the logits for the answers"""
+        image_features = self.model.encode_image(image)
+
+        question_features = self.model.encode_text(question_tokens)
+
+        answer_features = torch.stack([self.model.encode_text(batch_answers_tokens) for batch_answers_tokens in answer_tokens]).to(torch.float32)
+        
+        answer_features /= answer_features.clone().norm(dim=-1, keepdim=True)
+
+        # concatenate the features
+        combined_features = torch.cat((image_features, question_features), dim=1).to(self.device)  
+        combined_features /= combined_features.clone().norm(dim=-1, keepdim=True) 
+        combined_features = combined_features.to(torch.float32)
+        combined_features = self.fc1(combined_features)
+        combined_features = torch.nn.functional.relu(combined_features)
+        
+        # here normalization?
+        if self.device == 'cpu':
+            similarity = torch.einsum("bn,bqn->bq", [combined_features, answer_features])
+        else:
+            similarity = (100*torch.einsum("bn,bqn->bq", [combined_features, answer_features])) #scaling before softmax
+        
+        return similarity
+
+class VQA_Model4(torch.nn.Module):
+    """architecture that uses clip and a small NN to combine question and image features to get a embedding of the same size as the answer embedding
+        Only train the NN, not the clip model
+        with relu activation
+    """
+    def __init__(self, model, device):
+        super().__init__()
+        self.model = model
+        self.device = device
+        self.fc1 = torch.nn.Linear(1024, 512).to(self.device)
+        self.fc2 = torch.nn.Linear(512, 512).to(self.device)
+        self.initialize_parameters()
+
+    def initialize_parameters(self):
+        # Apply Xavier/Glorot initialization to the linear layer
+        torch.nn.init.xavier_uniform_(self.fc1.weight)
+        torch.nn.init.zeros_(self.fc1.bias.data)
+        torch.nn.init.xavier_uniform_(self.fc2.weight)
+        torch.nn.init.zeros_(self.fc2.bias.data)
+
+    def forward(self, image, question_tokens, answer_tokens):
+        """returns the logits for the answers"""
+        image_features = self.model.encode_image(image)
+
+        question_features = self.model.encode_text(question_tokens)
+
+        answer_features = torch.stack([self.model.encode_text(batch_answers_tokens) for batch_answers_tokens in answer_tokens]).to(torch.float32)
+        
+        answer_features /= answer_features.clone().norm(dim=-1, keepdim=True)
+
+        # concatenate the features
+        combined_features = torch.cat((image_features, question_features), dim=1).to(self.device)  
+        combined_features /= combined_features.clone().norm(dim=-1, keepdim=True) 
+        combined_features = combined_features.to(torch.float32)
+        combined_features = self.fc1(combined_features)
+        combined_features = torch.nn.functional.relu(combined_features)
+        combined_features = self.fc2(combined_features)
+        combined_features = torch.nn.functional.relu(combined_features)
+        
+        # here normalization?
+        if self.device == 'cpu':
+            similarity = torch.einsum("bn,bqn->bq", [combined_features, answer_features])
+        else:
+            similarity = (100*torch.einsum("bn,bqn->bq", [combined_features, answer_features])) #scaling before softmax
+        
+        return similarity
+
+class VQA_Model_Precalc(torch.nn.Module):
+    """Model that uses already calulated features"""
+    def __init__(self, model, device):
+        super().__init__()
+        self.model = model
+        self.device = device
+        self.fc1 = torch.nn.Linear(1024, 512).to(self.device)
+        self.initialize_parameters()
+
+    def initialize_parameters(self):
+        # Apply Xavier/Glorot initialization to the linear layer
+        torch.nn.init.xavier_uniform_(self.fc1.weight)
+        torch.nn.init.zeros_(self.fc1.bias.data)
+
+    def forward(self, image_features, question_features, answer_features):
+        """returns the logits for the answers"""
+        # concatenate the features
+        combined_features = torch.cat((image_features, question_features), dim=1).to(self.device)  
+        combined_features /= combined_features.clone().norm(dim=-1, keepdim=True) 
+        combined_features = combined_features.to(torch.float32)
+        combined_features = self.fc1(combined_features)
+        combined_features = torch.nn.functional.relu(combined_features)
+        
+        # here normalization?
+        if self.device == 'cpu':
+            similarity = torch.einsum("bn,bqn->bq", [combined_features, answer_features])
+        else:
+            similarity = (100*torch.einsum("bn,bqn->bq", [combined_features, answer_features])) #scaling before softmax
+        
+        return similarity
+    
